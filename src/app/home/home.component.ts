37d9a3ef16013c9d4fe9c559dc7b7ed941cb8fce
ZainAsif767 <zainasif7345@gmail.com>
2024-01-11 16:11:46 +0500

    home component implemented

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { HousingService } from './../housing.service';
+import { Component, inject } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+
+  constructor() {
+
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    })
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
+
