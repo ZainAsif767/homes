7707526a2c22c71b03f3110cdb46c22c2b145c12
ZainAsif767 <zainasif7345@gmail.com>
2024-01-11 16:10:31 +0500

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: false,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
